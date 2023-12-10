5ae25f1242142f4ece10cd3ace74f94c20aee847
David Barnes <dbarnes@kvcc.edu>
2023-12-10 18:53:38 -0500

    Finish day 6 part 1.

--- day6/day6.py
+++ day6/day6.py
@@ -1,4 +1,5 @@
 """Day 6 module"""
+import re
 
 
 class Day6:
@@ -24,6 +25,45 @@ class Day6:
 
     def solve_problem_1(self):
         """Solve problem 1"""
+
+        # Race list
+        races = []
+        # Get the lines of input
+        lines = self.file_reader.read_lines("day6/input.txt")
+        # For each line start processing line
+        race_time_line = lines[0]
+        race_distance_line = lines[1]
+
+        time_data = race_time_line.split(":")[1]
+        time_data = re.sub(" +", " ", time_data)[1:]
+        time_data = time_data.split(" ")
+
+        distance_data = race_distance_line.split(":")[1]
+        distance_data = re.sub(" +", " ", distance_data)[1:]
+        distance_data = distance_data.split(" ")
+
+        for i, time in enumerate(time_data):
+            races.append(
+                {
+                    "time": int(time),
+                    "distance": int(distance_data[i]),
+                    "ways_to_win": 0,
+                }
+            )
+
+        for entry in races:
+            time = entry["time"]
+            distance = entry["distance"]
+            for i in range(entry["time"]):
+                answer = i * (time - i)
+                if answer > distance:
+                    entry["ways_to_win"] += 1
+
+        total = 1
+        for entry in races:
+            total = total * entry["ways_to_win"]
+
+        self.ui.print_success(f"The total is: {total}")
 
     def solve_problem_2(self):
         """Solve problem 2"""
